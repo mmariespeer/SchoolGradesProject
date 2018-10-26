993f4d1bb6dc46d8bd1fa0c767961f431e55cb24
Melissa Speer Osborne <mspeer1@my.westga.edu>
2018-10-26 08:39:00 -0400

    Strategy testing complete, better naming conventions

--- src/test/java/edu/westga/cs/schoolgrades/TestCompositeGradeAverageStrategy.java
+++ src/test/java/edu/westga/cs/schoolgrades/TestCompositeGradeAverageStrategy.java
@@ -4,47 +4,47 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class TestCompositeGradeSumStrategy {
+public class TestCompositeGradeAverageStrategy {
 
 	@Test
-	public void testCompositeGradeAdd1GradeSum() {
-		Sum theSumStrategy = new Sum();
+	public void testCompositeGradeAdd1GradeAverage() {
+		Average theAverageStrategy = new Average();
 		SimpleGrade aNewGrade = new SimpleGrade(75.0);
-		CompositeGrade theGradeList = new CompositeGrade(theSumStrategy);
+		CompositeGrade theGradeList = new CompositeGrade(theAverageStrategy);
 		theGradeList.addGradeToCompositeList(aNewGrade);
 		assertEquals(75, theGradeList.getValue(), 75);
 	}
 	
 	@Test
-	public void testCompositeGradeAddMultipleGradesSum() {
-		Sum theSumStrategy = new Sum();
+	public void testCompositeGradeAddMultipleGradesAverage() {
+		Average theAverageStrategy = new Average();
 		SimpleGrade aNewGrade = new SimpleGrade(75.0);
 		SimpleGrade aNewGradeTwo = new SimpleGrade(80.0);
 		SimpleGrade aNewGradeThree = new SimpleGrade(95.0);
 		SimpleGrade aNewGradeFour = new SimpleGrade(100.0);
-		CompositeGrade theGradeList = new CompositeGrade(theSumStrategy);
+		CompositeGrade theGradeList = new CompositeGrade(theAverageStrategy);
 		theGradeList.addGradeToCompositeList(aNewGrade);
 		theGradeList.addGradeToCompositeList(aNewGradeTwo);
 		theGradeList.addGradeToCompositeList(aNewGradeThree);
 		theGradeList.addGradeToCompositeList(aNewGradeFour);
-		assertEquals(350, theGradeList.getValue(), 0);
+		assertEquals(87.5, theGradeList.getValue(), 0);
 	}
 	
 	@Test
-	public void testCompositeGradeAddMultipleGradesWithZeroSum() {
-		Sum theSumStrategy = new Sum();
+	public void testCompositeGradeAddMultipleGradesWithZeroAverage() {
+		Average theAverageStrategy = new Average();
 		SimpleGrade aNewGrade = new SimpleGrade(75.0);
 		SimpleGrade aNewGradeTwo = new SimpleGrade(80.0);
 		SimpleGrade aNewGradeThree = new SimpleGrade(95.0);
 		SimpleGrade aNewGradeFour = new SimpleGrade(100.0);
 		SimpleGrade aNewGradeFive = new SimpleGrade(0);
-		CompositeGrade theGradeList = new CompositeGrade(theSumStrategy);
+		CompositeGrade theGradeList = new CompositeGrade(theAverageStrategy);
 		theGradeList.addGradeToCompositeList(aNewGrade);
 		theGradeList.addGradeToCompositeList(aNewGradeTwo);
 		theGradeList.addGradeToCompositeList(aNewGradeThree);
 		theGradeList.addGradeToCompositeList(aNewGradeFour);
 		theGradeList.addGradeToCompositeList(aNewGradeFive);
-		assertEquals(350, theGradeList.getValue(), 0);
+		assertEquals(70, theGradeList.getValue(), 0);
 	}
 
 }
