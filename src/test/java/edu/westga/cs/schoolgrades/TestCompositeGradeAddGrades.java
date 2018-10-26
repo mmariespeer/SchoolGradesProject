037f088946e05c615b6b692a4717c0f9b59945bb
Melissa Speer Osborne <mspeer1@my.westga.edu>
2018-10-26 08:15:04 -0400

    CompositeGrade Setting methods completely tested

--- src/test/java/edu/westga/cs/schoolgrades/TestCompositeGradeAddGrades.java
+++ src/test/java/edu/westga/cs/schoolgrades/TestCompositeGradeAddGrades.java
@@ -15,5 +15,24 @@ public class TestCompositeGradeAddGrades {
 		assertEquals(1, theGradeList.getCompositeGradeList().size());
 		assertEquals(75.0, theGradeList.getCompositeGradeList().get(0).getValue(), 0);
 	}
+	
+	@Test
+	public void testCompositeGradeAddMultipleGrades() {
+		Sum theSumStrategy = new Sum();
+		SimpleGrade aNewGrade = new SimpleGrade(75.0);
+		SimpleGrade aNewGradeTwo = new SimpleGrade(80.0);
+		SimpleGrade aNewGradeThree = new SimpleGrade(95.0);
+		SimpleGrade aNewGradeFour = new SimpleGrade(100.0);
+		CompositeGrade theGradeList = new CompositeGrade(theSumStrategy);
+		theGradeList.addGradeToCompositeList(aNewGrade);
+		theGradeList.addGradeToCompositeList(aNewGradeTwo);
+		theGradeList.addGradeToCompositeList(aNewGradeThree);
+		theGradeList.addGradeToCompositeList(aNewGradeFour);
+		assertEquals(4, theGradeList.getCompositeGradeList().size());
+		assertEquals(75.0, theGradeList.getCompositeGradeList().get(0).getValue(), 0);
+		assertEquals(80.0, theGradeList.getCompositeGradeList().get(1).getValue(), 0);
+		assertEquals(95.0, theGradeList.getCompositeGradeList().get(2).getValue(), 0);
+		assertEquals(100.0, theGradeList.getCompositeGradeList().get(3).getValue(), 0);
+	}
 
 }
